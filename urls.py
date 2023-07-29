a5d3e59e50cb5fa166bfe67c51828540f1918e5d
Joshua Edgcombe <joshedgcombe@gmail.com>
2023-07-29 01:42:51 -0500

    Update to work with the latest version of django

--- urls.py
+++ urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import patterns, include, url
-
+# from django.urls import patterns, include, url
+from django.urls import include, path
 from home import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-)
+# urlpatterns = patterns('',
+#     url(r'^$', views.index, name='index'),
+# )
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
--- views.py
+++ views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 def index(request):
     from django.conf import settings
-    return render_to_response('home_index.html', {
+    return render(
+        request,
+        'home_index.html',
+        {
             'Apps': sorted(
                 [app for app in settings.INSTALLED_APPS if not "django" in app],
                 key=str.lower,
-                )
-            })
+            ),
+        },
+    )
